2fcb95e73273c75f9c3c85fb1f2e986ada3e668e
dachg <dachago1026@gmail.com>
2023-05-12 09:18:30 -0500

    Se agregan los modelos de producto, Direccion, CategoriaProducto y Factura

--- speedoColombia/orders/models.py
+++ speedoColombia/orders/models.py
@@ -8,7 +8,7 @@ class Departamento(models.Model):
 
 class Municipio(models.Model):
     municipio_id = models.IntegerField()
-    departamento_id = models.IntegerField()
+    departamento_id = models.ForeignKey()
     code = models.IntegerField()
     name = models.CharField(max_length=200)
 
@@ -19,8 +19,25 @@ class CategoriaProducto(models.Model):
 
 class Direccion(models.Model):
     direccion_id = models.IntegerField()
-    usuario_id = models.IntegerField()
-    municipio_id = models.IntegerField()
+    usuario_id = models.ForeignKey()
+    municipio_id = models.ForeignKey()
     direccion = models.CharField(max_length=200)
     direccion_actual = models.BooleanField
+
+class Producto(models.Model):
+    producto_id = models.IntegerField()
+    categoria_producto_id = models.ForeignKey()
+    orden_date = models.DateField()
+    nombre = models.CharField(max_length=200)
+    precio = models.FloatField()    
+
+class Factura(models.Model):
+    factura_id = models.IntegerField()
+    usuario_id = models.ForeignKey()
+    direcion_id = models.ForeignKey()
+    fecha_factura = models.DateField()
+    valor_total = models.IntegerField()
+
+
+
 
